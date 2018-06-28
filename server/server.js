2375a692b089f3fa9ecbb31959df3110aaae493e
Richard <catrosits@gmail.com>
2018-06-28 17:38:32 +0800

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const express = require('express');
-
-var app = express();
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
+
+var app = express();
+
+var server = require('http').createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('Server running in port 3000');
+
+    socket.on('disconnect', (socket) => {
+        console.log('Server is disconnected');
+    });
+})
+server.listen(port);
 app.use(express.static(publicPath));
-
-app.listen(port);
-console.log('Server running in port 3000');
-
