427fed571a093d234a8b8b4424b32354302f1d7f
Richard <catrosits@gmail.com>
2018-06-28 18:40:33 +0800

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,6 @@ var socket = io();
 socket.on('connect', function() {
     console.log('connected to server');
 
-    // socket.emit('createEmail', {
-    //     to: 'admin@test.com',
-    //     text: 'Hello world, this is new email.'
-    // });
-
-    socket.emit('createMessage', {
-        from: 'admin@test.com',
-        text: 'New message for you.'
-    })
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -13,13 +13,7 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('Server running in port 3000');
 
-    socket.emit('newEmail', {
-        from: 'anna@devjunction.com',
-        text: 'Hello there',
-        createAt: 123
-    });
-
-    socket.on('createEmail', (newEmail) => {
+        socket.on('createEmail', (newEmail) => {
         console.log('create Email', newEmail);
     });
 
@@ -27,14 +21,14 @@ io.on('connection', (socket) => {
         console.log('Server is disconnected');
     });
 
-    socket.emit('newMessage', {
-        from: 'me@sample.com',
-        text: 'Hey can we meet at 6',
-        createAt: 234
-    });
-
+    
     socket.on('createMessage', (newMessage) => {
         console.log('createMessage', newMessage);
+        io.emit('newMessage', {
+            from: 'message.from',
+            text: message.text,
+            createdAt: new DataCue().getTime()
+        });
     });
 })
 server.listen(port);
